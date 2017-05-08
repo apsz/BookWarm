ff0b7e75fa5ec15464dec41d3875ed07093e5c56
apsz <a.zych@student.uw.edu.pl>
2017-05-08 20:56:00 +0200

    bookwarm logic wip
    sqlalchemy for storage handling, wip client-server

--- bookwarm_client.py
+++ bookwarm_client.py
@@ -2,26 +2,97 @@
 
 
 import asyncio
+import getpass
+
+import CmdUtils
 
 
 class BookWarmClient(asyncio.Protocol):
-    message = 'Hello'
+
+    def __init__(self, user):
+        self._user = user
+        self._menus = dict(main_menu=self._main_menu_options,
+                           books_menu=self._books_menu_options,
+                           collections_menu=self._collections_menu_options)
+
+    @property
+    def user(self):
+        return self._user
 
     def connection_made(self, transport):
-        transport.write(self.message.encode())
-        print('data sent: {}'.format(self.message))
+        self._transport = transport
+        self._write(self._user)
 
-    def data_received(self, data):
-        print('data received: {}'.format(data.decode()))
+    def data_received(self, raw_data):
+        decoded_data = raw_data.decode('utf-8')
+
+        status, command, reply = decoded_data.split('  ')
+        self._handle_server_data(status, command, reply)
 
     def connection_lost(self, exc):
-        print('server closed the connection')
+        print('Server closed the connection.')
         asyncio.get_event_loop().stop()
+
+    def _handle_server_data(self, status, command, reply):
+        if status not in {'OK', 'RE'}:
+            print(reply)
+            self.quit()
+
+        if status == 'RE':
+            print(reply)
+
+        self._menus[command]()
+
+    def _main_menu_options(self):
+        user_choice = CmdUtils.get_str('(A)ll books  (M)y Collections  (Q)uit',
+                                       input_type='option', valid='amq')
+        self._send_formatted(command=user_choice, client_data='', options_menu='main_menu',
+                             next_menu='main_menu')
+
+    def _books_menu_options(self):
+        user_choice = CmdUtils.get_str('(A)dd Book  (V)iew  (E)dit  (D)elete  (B)ack',
+                                       input_type='option', valid='avedb')
+        if user_choice == 'b':
+            self._main_menu_options()
+        else:
+            self._send_formatted(command=user_choice, client_data='',
+                                 options_menu='collections_menu', next_menu='collections_menu')
+
+    def _collections_empty_options(self):
+        user_choice = CmdUtils.get_str('(C)reate  (B)ack', input_type='option', valid='cb')
+        if user_choice == 'c':
+            collection_name = CmdUtils.get_str('Collection name', input_type='string', min_len=1)
+            self._send_formatted(command=user_choice, client_data=collection_name,
+                                 options_menu='collections_menu', next_menu='collections_menu')
+        else:
+            self._main_menu_options()
+
+    def _collections_menu_options(self):
+        user_choice = CmdUtils.get_str('(A)dd Collection  (V)iew  (E)dit  (D)elete  (B)ack',
+                                       input_type='option', valid='vedb')
+        self._send_formatted(command=user_choice, client_data='', options_menu='collections_menu',
+                             next_menu='collections_menu')
+
+    def quit(self, *ignore):
+        asyncio.get_event_loop().stop()
+
+    def _write(self, text):
+        self._transport.write(text.encode('utf-8'))
+
+    def _send_formatted(self, command, client_data, options_menu,
+                        next_menu, format='{}  {}  {}  {}'):
+        self._write(format.format(command, client_data, options_menu, next_menu))
+
+
+def get_user():
+    return getpass.getuser()
 
 
 def main():
+    user = get_user()
     loop = asyncio.get_event_loop()
-    coro = loop.create_connection(BookWarmClient, 'localhost', 23)
+    coro = loop.create_connection(lambda: BookWarmClient(user),
+                                  'localhost', 23)
     loop.run_until_complete(coro)
     loop.run_forever()
     loop.close()
--- bookwarm_server.py
+++ bookwarm_server.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 
+import os
 import sys
 import argparse
 import asyncio
+
+import bookwarm
 from bookwarm_serverproto import ServerProtocol
 
 
@@ -14,8 +17,13 @@ class BookWarmServer:
         self._host = host
         self._port = port
         self._loop = loop
-        self._user_collections = []
-        self._all_books = {}
+        self._active_users = set()
+        self._database_path = self._setup_database()
+        self.__all_books = self._load_all_available_books()
+
+    @property
+    def all_books(self):
+        return self.__all_books
 
     def run(self):
         serv_coro = self._loop.create_server(
@@ -23,6 +31,57 @@ class BookWarmServer:
             host=self._host,
             port=self._port)
         return self._loop.run_until_complete(serv_coro)
+
+    def add_user(self, new_user):
+        if new_user in self._active_users:
+            return False
+        self._active_users.add(new_user)
+        return True
+
+    def remove_user(self, user):
+        self._active_users.remove(user)
+
+    def get_user_collections(self, user):
+        with bookwarm.SQLSession(self._database_path) as session:
+            return session.query(bookwarm.BookCollection).filter(
+                bookwarm.BookCollection.user == user).all()
+
+    def get_collection_by_name(self, collection_name):
+        with bookwarm.SQLSession(self._database_path) as session:
+            return session.query(bookwarm.BookCollection).filter(
+                bookwarm.BookCollection.collection_name == collection_name).one()
+
+    def find_book_by_isbn(self, isbn):
+        with bookwarm.SQLSession(self._database_path) as session:
+            return session.query(bookwarm.Book).filter(
+                bookwarm.Book.isbn == isbn).one()
+
+    def add_new_book(self, isbn):
+        if not self.find_book_by_isbn(isbn):
+            pass
+
+    def add_new_collection(self, user, collection_name):
+        new_collection = bookwarm.BookCollection(user=user,
+                                                 collection_name=collection_name,
+                                                 book_collection=[])
+
+    def _setup_database(self):
+        try:
+            data_folder = os.path.join(os.path.dirname(__file__), 'data')
+            if not os.path.exists(data_folder):
+                os.mkdir(data_folder)
+            database_file_path = os.path.join(data_folder, 'bookwarm.db')
+            if not os.path.exists(database_file_path):
+                bookwarm.setup_database(database_file_path)
+            return database_file_path
+        except (EnvironmentError, IOError) as data_setup_err:
+            print('Server cannot create necessary database folder/file: {}\n'
+                  'Exiting...'.format(data_setup_err))
+            sys.exit()
+
+    def _load_all_available_books(self):
+        with bookwarm.SQLSession(self._database_path) as session:
+            self.__all_books = session.query(bookwarm.UserBook).all()
 
 
 def get_args():
